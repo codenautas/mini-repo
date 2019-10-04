0f3dfb3bc3c5f3207a733996dc8124590b1e33c4
Emilio <emilioplatzer@gmail.com>
2019-10-04 11:23:12 -0300

    Anda unlogged

--- src/unlogged/unlogged.ts
+++ src/unlogged/unlogged.ts
@@ -4,10 +4,16 @@ import {mostrar} from "./matriz";
 
 window.addEventListener('load', async function(){
     var layout = document.getElementById('total-layout')!;
+    if(!layout){
+        console.log('no encuentro el DIV.total-layout')
+        await myOwn.ready;
+        layout = document.getElementById('total-layout')!;
+    }
     layout.innerHTML+='<div>...</div>';
     // @ts-ignore ready existe!
     await myOwn.ready;
     layout.innerHTML+='<div>_</div>';
     var result = await myOwn.ajax.matriz_traer({});
+    layout.innerHTML='<div id=main_layout></div>'
     mostrar(result);
 })
