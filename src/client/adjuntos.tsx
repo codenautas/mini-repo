1886c2061bd9a85225efdf1ce66c1c7e603815d0
Manuel De la Penna <manueldelapenna@gmail.com>
2019-10-09 10:45:11 -0300

    agrego espacio

--- src/client/adjuntos.tsx
+++ src/client/adjuntos.tsx
@@ -17,15 +17,15 @@ const ImagenInfo = function(props:{src:string|undefined, sizeParams:ParametrosIm
             <div id="image-info">
                 <div id="image-width-validation">
                     ancho: {width}px 
-                    <span className={validWidth?'success':'danger'}>{validWidth?VALID:INVALID + ` (el ancho recomendado es de ${props.sizeParams.min_width_px_imagen_matriz} hasta ${props.sizeParams.max_width_px_imagen_matriz} px)`}</span>
+                    <span className={validWidth?'success':'danger'}> {validWidth?VALID:INVALID + ` (el ancho recomendado es de ${props.sizeParams.min_width_px_imagen_matriz} hasta ${props.sizeParams.max_width_px_imagen_matriz} px)`}</span>
                 </div>
                 <div id="image-heigth-validation">
                     alto: {height}px 
-                    <span className={validHeight?'success':'danger'}>{validHeight?VALID:INVALID + ` (la altura recomendada es de ${props.sizeParams.min_height_px_imagen_matriz} hasta ${props.sizeParams.max_height_px_imagen_matriz} px)`}</span> 
+                    <span className={validHeight?'success':'danger'}> {validHeight?VALID:INVALID + ` (la altura recomendada es de ${props.sizeParams.min_height_px_imagen_matriz} hasta ${props.sizeParams.max_height_px_imagen_matriz} px)`}</span> 
                 </div>
                 <div id="image-aspect-ratio-validation">
                     aspect ratio: {Math.round(width/height * 100) / 100} 
-                    <span className={validAspectRatio?'success':'danger'}>{validAspectRatio?VALID:INVALID + ` (el aspect ratio recomendado es de ${props.sizeParams.min_aspect_ratio_imagen_matriz} hasta ${props.sizeParams.max_aspect_ratio_imagen_matriz})`}</span> 
+                    <span className={validAspectRatio?'success':'danger'}> {validAspectRatio?VALID:INVALID + ` (el aspect ratio recomendado es de ${props.sizeParams.min_aspect_ratio_imagen_matriz} hasta ${props.sizeParams.max_aspect_ratio_imagen_matriz})`}</span> 
                 </div>
             </div>
         )
