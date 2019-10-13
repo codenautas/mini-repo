0bf60f4cdc9e625e62c914a6e61f0bfc3ed4ae73
Emilio Platzer <emilioplatzer@gmail.com>
2019-10-13 17:21:51 -0300

    Agrego un snakbar para indicar que no hay resultados en la búsqueda

--- src/unlogged/matriz.tsx
+++ src/unlogged/matriz.tsx
@@ -21,6 +21,7 @@ import {
     ListItem,
     ListItemText,
     Slide,
+    Snackbar,
     SvgIcon,
     SwipeableDrawer,
     Toolbar,
@@ -68,14 +69,27 @@ import {
 // https://material-ui.com/components/material-icons/
 export const materialIoIconsSvgPath:{[k:string]:string}={
     Assignment: "M19 3h-4.18C14.4 1.84 13.3 1 12 1c-1.3 0-2.4.84-2.82 2H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm-7 0c.55 0 1 .45 1 1s-.45 1-1 1-1-.45-1-1 .45-1 1-1zm2 14H7v-2h7v2zm3-4H7v-2h10v2zm0-4H7V7h10v2z",
+    Close: "M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z",
     Code: "M9.4 16.6L4.8 12l4.6-4.6L8 6l-6 6 6 6 1.4-1.4zm5.2 0l4.6-4.6-4.6-4.6L16 6l6 6-6 6-1.4-1.4z",
     EmojiObjects: "M12 3c-.46 0-.93.04-1.4.14-2.76.53-4.96 2.76-5.48 5.52-.48 2.61.48 5.01 2.22 6.56.43.38.66.91.66 1.47V19c0 1.1.9 2 2 2h.28c.35.6.98 1 1.72 1s1.38-.4 1.72-1H14c1.1 0 2-.9 2-2v-2.31c0-.55.22-1.09.64-1.46C18.09 13.95 19 12.08 19 10c0-3.87-3.13-7-7-7zm2 16h-4v-1h4v1zm0-2h-4v-1h4v1zm-1.5-5.59V14h-1v-2.59L9.67 9.59l.71-.71L12 10.5l1.62-1.62.71.71-1.83 1.82z",
-    Label:"M17.63 5.84C17.27 5.33 16.67 5 16 5L5 5.01C3.9 5.01 3 5.9 3 7v10c0 1.1.9 1.99 2 1.99L16 19c.67 0 1.27-.33 1.63-.84L22 12l-4.37-6.16z",
+    Info: "M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 15h-2v-6h2v6zm0-8h-2V7h2v2z",
+    Label: "M17.63 5.84C17.27 5.33 16.67 5 16 5L5 5.01C3.9 5.01 3 5.9 3 7v10c0 1.1.9 1.99 2 1.99L16 19c.67 0 1.27-.33 1.63-.84L22 12l-4.37-6.16z",
     LocalAtm: "M11 17h2v-1h1c.55 0 1-.45 1-1v-3c0-.55-.45-1-1-1h-3v-1h4V8h-2V7h-2v1h-1c-.55 0-1 .45-1 1v3c0 .55.45 1 1 1h3v1H9v2h2v1zm9-13H4c-1.11 0-1.99.89-1.99 2L2 18c0 1.11.89 2 2 2h16c1.11 0 2-.89 2-2V6c0-1.11-.89-2-2-2zm0 14H4V6h16v12z",
     KeyboardArrowUp: "M7.41 15.41L12 10.83l4.59 4.58L18 14l-6-6-6 6z",
-    Menu:"M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z",
-    SearchIcon:"M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"
+    Menu: "M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z",
+    SearchIcon: "M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z",
+    Warning: "M1 21h22L12 2 1 21zm12-3h-2v-2h2v2zm0-4h-2v-4h2v4z",
 }
+
+const CloseIcon = () =>
+    <SvgIcon>
+        <path d={materialIoIconsSvgPath.Close} />
+    </SvgIcon>
+
+const InfoIcon = () =>
+    <SvgIcon>
+        <path d={materialIoIconsSvgPath.Info} />
+    </SvgIcon>
 
 const MenuIcon = () =>
     <SvgIcon>
@@ -85,6 +99,11 @@ const MenuIcon = () =>
 const SearchIcon = () =>
     <SvgIcon>
         <path d={materialIoIconsSvgPath.SearchIcon} />
+    </SvgIcon>
+
+const WarningIcon = () =>
+    <SvgIcon>
+        <path d={materialIoIconsSvgPath.Warning} />
     </SvgIcon>
 
 const KeyboardArrowUpIcon = () =>
@@ -160,7 +179,7 @@ function HideOnScroll(props: { children: JSX.Element }) {
     );
 }
 
-const handleScroll = (id:string) => (event: React.MouseEvent<HTMLDivElement>) => {
+const handleScroll = (id:string) => () => {
     // window.scrollTo(0,0)
     const anchor = document.getElementById(id);
     if (anchor) {
@@ -170,19 +189,18 @@ const handleScroll = (id:string) => (event: React.MouseEvent<HTMLDivElement>) =>
     }
 };
 
-function SearchAppBar(props: { dimensiones:Dimension[], onSearch: (value: string) => void }) {
-    var [search, setSearch] = useState();
+function SearchAppBar(props: { dimensiones:Dimension[], search:string|null, onSearch: (value: string) => void }) {
+    var search = props.search;
     var [menuAbierto, setMenuAbierto] = useState(false);
     var [irA, setIrA] = useState<string|null>(null);
     React.useEffect(()=>{
         if(irA){
-            handleScroll("dimension-"+irA)(null);
+            handleScroll("dimension-"+irA)();
             setIrA(null)
         }
     },[irA])
     const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         const value = event.target.value;
-        setSearch(value);
         props.onSearch(value);
     };
     const classes = useStylesSearchAppBar();
@@ -202,6 +220,7 @@ function SearchAppBar(props: { dimensiones:Dimension[], onSearch: (value: string
                         </IconButton>
                         <Typography className={classes.title} variant="h6" noWrap>
                             Banco de Datos
+                            <img class="local-logo" src="./storage/local-logo.png" />
                         </Typography>
                         <div className={classes.search}>
                             <div className={classes.searchIcon}>
@@ -410,19 +429,59 @@ function AppMiniRepo(props:{dimensiones:Dimension[]}){
     const [search, setSearch] = useState<string|null>(null);
     const searchChange = setSearch;
     const filteredResult = props.dimensiones.map(dimension=>
-        search!=null?{...dimension, indicadores:dimension.indicadores.filter(indicador=>
+        !search?dimension:{...dimension, indicadores:dimension.indicadores.filter(indicador=>
             search.split(' ').map(p=>p.toLocaleLowerCase()).every(palabraBuscada=>
                 indicador.palabras.map(p=>p.toLocaleLowerCase()).filter(palabra=>palabra.includes(palabraBuscada)).length
             )
-        )}:dimension
+        )}
     ).filter(dimension=>
         dimension.indicadores.length
-    )
+    );
+    var resultCount=filteredResult.reduce((sum,dimension)=>sum+dimension.indicadores.length,0);
     return <React.StrictMode>
         <CssBaseline />
         <div className="matriz-comparacion">
-            <SearchAppBar dimensiones={props.dimensiones} onSearch={searchChange}/>
+            <SearchAppBar dimensiones={props.dimensiones} search={search} onSearch={searchChange}/>
             <ListaIndicadores dimensiones={filteredResult}/>
+            {!resultCount?
+                    <Snackbar
+                        anchorOrigin={{
+                            vertical: 'bottom',
+                            horizontal: 'right',
+                        }}
+                        open={!resultCount}
+                        ContentProps={{
+                            'aria-describedby': 'message-id',
+                        }}
+                        message={<span id="client-snackbar">
+                            <WarningIcon/> 
+                            <span id="message-id">No hay datos que coincidan con la búsqueda</span>
+                        </span>}
+                        action={[
+                            <Button key="undo" color="secondary" size="small" onClick={()=>setSearch('')}>
+                                LIMPIAR
+                            </Button>,
+                        ]}
+                    />                
+            :null}
+            {search=="#"?
+                <Snackbar
+                    anchorOrigin={{
+                        vertical: 'bottom',
+                        horizontal: 'left',
+                    }}
+                    open={!resultCount}
+                    ContentProps={{
+                        'aria-describedby': 'message-id',
+                    }}
+                    message={<span id="client-snackbar">
+                        <InfoIcon/> 
+                        <span id="message-id">{['innerWidth','outerWidth','innerHeight','outerHeight'].map(attr=>
+                            <span> <b> {attr}: </b> {window[attr]} </span> 
+                        )}</span>
+                    </span>}
+                />                
+            :null}
         </div>
         <ScrollTop>
             <Fab color="secondary" size="small" aria-label="scroll back to top">
